3e4929b6cdf0fdf3e8ef81fc90dfca374a1e599a
Jessica Winters <jskawinters@gmail.com>
2018-10-29 19:01:18 +0000

    Added Step 2 - Printing with Buffered Strings

--- buffered-strings.js
+++ buffered-strings.js
@@ -1,10 +1,10 @@
 // while https is built-in to Node, it is a module, so it must be required
 const https = require('https');
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
   const requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   // notice that https.get takes a callback with one parameter -
@@ -12,18 +12,21 @@ function getAndPrintHTMLChunks () {
   https.get(requestOptions, function (response) {
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
+    
+    let chunk = '';
 
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      console.log(data, '\n');
+      chunk += data;
+      return chunk;
     });
 
     // the callback is invoked when all of the data has been received
     // (the `end` of the stream)
     response.on('end', function() {
-      console.log('Response stream complete.');
+      console.log(chunk);
     });
   });
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
